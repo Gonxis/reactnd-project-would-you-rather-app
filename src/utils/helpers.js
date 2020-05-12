9eaa8c33db097548e9c5caa9112b4039ad18cdfb
Gonzalo Cercos <goncholo@gmail.com>
2020-05-12 02:58:45 +0200

    Feat: Adding the UI in Question component

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,3 +3,25 @@ export function formatDate(timestamp) {
     const time = d.toLocaleTimeString('en-US')
     return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
 }
+
+export function formatQuestion(question, author, authedUser) {
+    const { id, timestamp, optionOne, optionTwo } = question
+    const { name, avatarURL } = author
+    const votes = optionOne.votes.length + optionTwo.votes.length;
+
+    return {
+        name,
+        id,
+        timestamp,
+        avatar: avatarURL,
+        votes,
+        hasVotes: votes !== 0,
+        yourVote: optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser),
+        options: {
+            optionOneValue: optionOne.text,
+            optionOneVotes: [...optionOne.votes],
+            optionTwoValue: optionTwo.text,
+            optionTwoVotes: [...optionTwo.votes]
+        }
+    }
+}
